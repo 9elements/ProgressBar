b47baa4f025015d4e90451606e551b7e72b4b413
Marvin Drees <marvin.drees@9elements.com>
2022-07-15 17:36:45 +0200

    feat: add basic wifi support
    This adds the basic wifi code with esps wifi manager.
    It uses protocomm with the SoftAP provisioning app to
    securely transfer the wifi credentials over.
    
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

---
+++ main/wifi.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
+
+#include <wifi_provisioning/manager.h>
+#include <wifi_provisioning/scheme_softap.h>
+
+void provision_wifi(void);
