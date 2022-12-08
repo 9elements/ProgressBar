cdebf265727a9faae43080a9b5244ce2d9b0c375
Marvin Drees <marvin.drees@9elements.com>
2022-12-08 15:59:09 +0100

    refactor: remove provisioning manager
    Greatly simplify the codebase by removing the provisioning
    manager and moving the source into the main component
    
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

--- main/app_main.c
+++ main/app_main.c
@@ -8,7 +8,7 @@ void app_main(void) {
 
     esp_pm_config_esp32c3_t pm_config = {
         .max_freq_mhz = 160,
-        .min_freq_mhz = 40,
+        .min_freq_mhz = 20,
         .light_sleep_enable = true,
     };
     ESP_ERROR_CHECK(esp_pm_configure(&pm_config));
